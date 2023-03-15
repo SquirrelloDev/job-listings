88ec807885c3015dfa36a93a6ec877faf4a4e133
SquirrelloDev <olejnike2000@gmail.com>
2023-03-15 17:54:02 +0100

    Implemented fetching data file

--- src/components/JobOffers.js
+++ src/components/JobOffers.js
@@ -1,7 +1,9 @@
 import JobOffer from "./JobOffer/JobOffer";
 import classes from "../sass/components/JobOffers.module.scss";
+import useFileData from "../hooks/use-file-data";
 
 const JobOffers = () => {
+    const {fileData} = useFileData('./data.json');
   return(
       <div className={classes.jobs}>
           <JobOffer/>

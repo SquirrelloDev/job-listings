5cb4cf4ea7906b15c33121cbd66f0edd80d1ad2b
SquirrelloDev <olejnike2000@gmail.com>
2023-03-15 18:45:51 +0100

    Added fallback for clicking the filter multiple times

--- src/context/FilterProvider.js
+++ src/context/FilterProvider.js
@@ -2,12 +2,16 @@ import FiltersContext from "./filters-context";
 import {useState} from "react";
 const FilterProvider = ({children}) => {
     const [filters, setFilters] = useState([]);
-    const addFilter = (filter) => {
-      const newFilterArr = filters.concat(filter);
-      setFilters(newFilterArr);
-    }
     const removeFilter = (filter) => {
         const newFilterArr = filters.filter(item => item !== filter);
+        setFilters(newFilterArr);
+    }
+    const addFilter = (filter) => {
+        if(filters.includes(filter)){
+            removeFilter(filter);
+            return;
+        }
+        const newFilterArr = filters.concat(filter);
         setFilters(newFilterArr);
     }
     const clearFilters = () => {

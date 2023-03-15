5bc3016de867dae1a1d716b941952f9f014e73de
SquirrelloDev <olejnike2000@gmail.com>
2023-03-15 19:55:34 +0100

    Implemented filtering logic

--- src/components/JobOffers.js
+++ src/components/JobOffers.js
@@ -1,12 +1,27 @@
 import JobOffer from "./JobOffer/JobOffer";
 import classes from "../sass/components/JobOffers.module.scss";
 import useFileData from "../hooks/use-file-data";
+import {useContext, useEffect, useState} from "react";
+import filtersContext from "../context/filters-context";
 
 const JobOffers = () => {
     const {fileData} = useFileData('./data.json');
+    const filterCtx = useContext(filtersContext);
+    const [jobs, setJobs] = useState(fileData);
+    useEffect(() => {
+        if(filterCtx.filters.length <= 0){
+            setJobs(fileData);
+            return;
+        }
+        const filteredJobs = fileData.filter(job => {
+            let filterItems = [job.role,job.level, ...job.languages, ...job.tools];
+            return filterCtx.filters.every(item => filterItems.includes(item));
+        })
+        setJobs(filteredJobs);
+    },[fileData, filterCtx.filters])
   return(
       <div className={classes.jobs}>
-          {fileData.map(jobData => <JobOffer key={jobData.id} jobData={jobData}/> )}
+          {jobs.map(jobData => <JobOffer key={jobData.id} jobData={jobData}/> )}
       </div>
 
   )

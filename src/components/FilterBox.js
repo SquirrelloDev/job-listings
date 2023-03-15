97344718566a074d1120a2dda9f264e4c9415f03
SquirrelloDev <olejnike2000@gmail.com>
2023-03-15 20:01:14 +0100

    Final fixes. Project completed

--- src/components/FilterBox.js
+++ src/components/FilterBox.js
@@ -3,12 +3,11 @@ import Chip from "./Chip";
 import {useContext} from "react";
 import filtersContext from "../context/filters-context";
 
-const FilterBox = ({filters}) => {
+const FilterBox = () => {
     const filterCtx = useContext(filtersContext);
   return (
       <div className={classes.filter__container}>
           <div className={classes.filter__container__chips}>
-              {/*  TODO: Here will be the chip components*/}
               {filterCtx.filters.map(filter => <Chip>{filter}</Chip>)}
           </div>
                 <button onClick={filterCtx.clearFilters} className={classes.filter__container__clear}>Clear</button>

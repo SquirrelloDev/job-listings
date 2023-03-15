afe8821ff2aefc4c84c2be291a53264e2587fa6e
SquirrelloDev <olejnike2000@gmail.com>
2023-03-15 18:16:45 +0100

    Added rendering job perks

--- src/components/JobOffer/JobOffer.js
+++ src/components/JobOffer/JobOffer.js
@@ -1,13 +1,16 @@
 import classes from "../../sass/components/JobOffer.module.scss";
 import InfoChip from "./InfoChip";
 import PerkChip from "./PerkChip";
-import testImg from '../../images/insure.svg'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircle} from "@fortawesome/free-solid-svg-icons";
+import {useEffect, useState} from "react";
 const JobOffer = ({jobData}) => {
+    const [perks, setPerks] = useState([]);
     const {company, logo, new: isNew, featured: isFeatured, position, role, level, postedAt, contract, location, languages, tools} = jobData;
     let featuredClasses = isFeatured ? `${classes.offer} ${classes.featured}` : classes.offer;
-
+    useEffect(() =>{
+        setPerks([...languages, ...tools]);
+    }, [languages,tools])
   return(
       <div className={featuredClasses}>
           <div className={classes.offer__job}>
@@ -27,11 +30,9 @@ const JobOffer = ({jobData}) => {
 
           <hr/>
           <div className={classes.offer__chips}>
-            <PerkChip>CSS</PerkChip>
-              <PerkChip>React</PerkChip>
-              <PerkChip>Next</PerkChip>
-              <PerkChip>Sass</PerkChip>
-              <PerkChip>RWD</PerkChip>
+            <PerkChip>{role}</PerkChip>
+              <PerkChip>{level}</PerkChip>
+              {perks.map(perk => <PerkChip>{perk}</PerkChip>)}
           </div>
       </div>
   )

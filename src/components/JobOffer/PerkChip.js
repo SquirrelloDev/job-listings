a4f4c501cfaa219599fa01e4b619991c0d6b0993
SquirrelloDev <olejnike2000@gmail.com>
2023-03-15 18:53:00 +0100

    Added button highlighting when filter matches the button title

--- src/components/JobOffer/PerkChip.js
+++ src/components/JobOffer/PerkChip.js
@@ -4,8 +4,14 @@ import filtersContext from "../../context/filters-context";
 
 const PerkChip = ({children}) => {
   const filterCtx = useContext(filtersContext);
+  let chipClasses = classes['perk-chip'];
+  filterCtx.filters.forEach(filter =>{
+    if(filter === children){
+      chipClasses = `${classes['perk-chip']} ${classes['perk-chip__active']}`
+    }
+  })
   return (
-      <button onClick={filterCtx.addFilter.bind(null, `${children}`)} className={classes['perk-chip']}>{children}</button>
+      <button onClick={filterCtx.addFilter.bind(null, `${children}`)} className={chipClasses}>{children}</button>
   )
 }
 export default PerkChip;
